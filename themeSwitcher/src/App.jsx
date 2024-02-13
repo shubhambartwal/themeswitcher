4dabcaeeb8c87a5763a6e8550edccb4f64cd55d7
Shubham Bartwal <57596569+shubhambartwal@users.noreply.github.com>
2024-02-13 12:18:08 +0000

    created context APi for UI change (light to dark vice varsa )

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,24 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
-
+import { ThemeProvider } from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 function App() {
-  
+  const [themeMode, setThemeMode] = useState("light")
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
-                    </div>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <Card />
+          <div className="w-full max-w-sm mx-auto">
 
-                    <div className="w-full max-w-sm mx-auto">
-                       
-                    </div>
-                </div>
-            </div>
-
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
